c842c609fc16cc91c899c65cdebd038071701ac7
Scott Morton <smorton@Scotts-MacBook-Pro.local>
2020-10-19 20:17:32 -0600

    staging changes before push

--- routes/dbhelperqueries.js
+++ routes/dbhelperqueries.js
@@ -13,7 +13,6 @@ const pool = new Pool({
 });
 // /// Users
 
-<<<<<<< HEAD
 // /**
 //  * Get a single user from the database given their email.
 //  * @param {String} email The email of the user.
@@ -45,39 +44,6 @@ const pool = new Pool({
 // };
 
 // exports.getUserWithId = getUserWithId;
-=======
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
-const getUserWithEmail = function (email) {
-  return pool.query(`SELECT *
- FROM users
- WHERE users.email = $1;`,
-    [email])
-    .then(res =>
-      res.rows[0]);
-};
-
-exports.getUserWithEmail = getUserWithEmail;
-
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
-const getUserWithId = function (id) {
-  return pool.query(`SELECT *
-    FROM users
-    WHERE users.id = $1;`,
-    [id])
-    .then(res =>
-      res.rows[0]);
-};
-
-exports.getUserWithId = getUserWithId;
->>>>>>> 9397d6a61a4095e19838b4398796311b539e1558
 
 
 // /**
@@ -85,7 +51,6 @@ exports.getUserWithId = getUserWithId;
 //  * @param {{name: string, password: string, email: string}} user
 //  * @return {Promise<{}>} A promise to the user.
 //  */
-<<<<<<< HEAD
 // const addUser = function (user) {
 //   return pool.query(`
 //   INSERT INTO users (name, email, password)
@@ -97,19 +62,6 @@ exports.getUserWithId = getUserWithId;
 // };
 
 // exports.addUser = addUser;
-=======
-const addUser = function (user) {
-  return pool.query(`
-  INSERT INTO users (name, email, password)
-  VALUES ($1, $2, $3)
-  RETURNING *`,
-    [user.name, user.email, user.password])
-    .then(res =>
-      res.rows);
-};
-
-exports.addUser = addUser;
->>>>>>> 9397d6a61a4095e19838b4398796311b539e1558
 
 // /// Reservations
 
