ac0f0e639459adebc309c585c6640669ab9091dc
davidsmorton <davidsmorton@gmail.com>
2020-10-23 23:16:56 +0000

    presentation push

--- routes/resources.js
+++ routes/resources.js
@@ -4,153 +4,94 @@
  *   these routes are mounted onto /widgets
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
-const { getUser, commentsForResourceId, getAllContent, contentView, addFavourite, addComment, addRating, addResource, updateUser } = require('../db/database_functions/database');
+const {
+  getUser,
+  commentsForResourceId,
+  getAllContent,
+  contentView,
+  addFavourite,
+  addComment,
+  addRating,
+  addResource,
+  updateUser,
+  myResources,
+} = require("../db/database_functions/database");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-
-  // router.get("/", (req, res) => {
-  //   let query = `SELECT * FROM resources`;
-  //   console.log(query);
-  //   db.query(query)
-  //     .then(data => {
-  //       const resources = data.rows;
-  //       res.json({ resources });
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
   //Filter for food
   router.get("/", (req, res) => {
-    console.log("im in router get /");
     console.log(getAllContent());
     getAllContent(req.query)
-      .then(resources => {
-        res.render('index', { resources });
+      .then((resources) => {
+        res.render("index", { resources });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
+//gets specific comments
   router.get("/:id/comments", (req, res) => {
-
     const { id } = req.params;
-
-    commentsForResourceId(id)
-      .then((rows) => {
-        res.json(rows);
-      }
-
-      );
+    commentsForResourceId(id).then((rows) => {
+      res.json(rows);
+    });
   });
+
   router.post("/:id/comments", (req, res) => {
     const { id: resource_id } = req.params;
     const user_id = 1;
     const comment = req.body.comment;
-    console.log("req.body>>>",req.body)
-    console.log("comment>>>>",comment)
-    const result = addComment({comment, resource_id, user_id })
-      result.then((createdComment) => {
-        console.log("created comment",createdComment)
-        res.send(createdComment);
-      }
-      );
+    console.log("req.body>>>", req.body);
+    console.log("comment>>>>", comment);
+    const result = addComment({ comment, resource_id, user_id });
+    result.then((createdComment) => {
+      console.log("created comment", createdComment);
+      res.send(createdComment);
+    });
   });
-
-
-
-
 
   router.get("/new_content", (req, res) => {
-    res.render("new_content");// ,templateVars;
+    res.render("new_content"); // ,templateVars;
   });
 
+  router.post("/new_content", (req, res) => {
+    addResource(req.body).then((resource) => {
+      res.redirect("/api/users");
+    });
+  });
 
-  router.post('/new_content', (req, res) => {
-    addResource(req.body)
-    .then(resource => {
-      res.redirect('/api/users')
-    })
-  })
+  // post for adding a rating to a resource_DSM
+  router.post("/:id/rating", (req, res) => {
+    const id = req.params.id;
+    const selectedRating = req.body.selectedRating;
 
-// post for adding a rating to a resource_DSM
-router.post('/:id/rating', (req, res) => {
+    addRating({ rating: selectedRating, resource_id: id });
+  });
 
-  // const {id: resource_id} = req.params;
-  const id = req.params.id;
-  const selectedRating = req.body.selectedRating;
-
-  addRating({rating: selectedRating, resource_id: id})
-  // const { rating } = req.body;
-  // addRating({rating, id, userid})
-  //grab resource id from params
-  //grab rating from body (name is "rating")
-  //grab user id from params
-  // db insert of resource_id, rating, user_id
-  //res.send results
-  // console.log("resource_id prams: ", resource_id);
-  // console.log("rating rating: ", rating);
-
-
-})
-
-
-//***** YOUR CREATED resource PAGE *****
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  contentView(id)// replace with addComment then use the response to append the the front end if i want to save in database
-    .then(resource => {
+  //***** individual view of a specific resource
+  router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    contentView(id).then((resource) => {
       const templateVars = {
         resource: resource,
-
       };
       res.render("ind_view.ejs", templateVars);
     });
-});
-
-
-
-
-
-  // router.post('/:id', (req, res) => {
-
-  // router.post('/:id', (req, res) => {
-  //   const { id } = req.params;
-  //   const comment = req.body
-  //     .then(comment => {
-  //       res.send(comment);
-  //     })
-  //     .catch(err => {
-  //       console.error(err);
-  //       res.send(err);
-  //     });
-  // });
-
-  // router.post('/:id', (req, res) => {
-
-  //   const { id } = req.params;
-  //   console.log("id>>>", id);
-  //   contentView(id);
-  //   const { comment } = req.body
-  //     .then(comment => {
-  //       res.send('ind_view', comment);
-  //     })
-  //     .catch(err => {
-  //       console.error(err);
-  //       res.send(err);
-  //     });
-  // });
-
+  });
+  //get all resources made by adn liked by a specific user
+  router.get("/:id/myresources", (req, res) => {
+    const user_id = req.params.id;
+    myResources(user_id).then((resources) => {
+      const templateVars = {
+        resources: resources,
+      };
+      res.render("index.ejs", templateVars);
+    });
+  });
 
   return router;
 };
-
